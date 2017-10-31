6593c74d817a8c9ee8f3fa9a308981d7e4d66b5e
Anne <anneomclaughlin@gmail.com>
2017-10-31 00:54:01 -0700

    Overhauling gameplay and clues/relations

--- game/data/fact_relations.gd
+++ game/data/fact_relations.gd
@@ -1,157 +1,109 @@
 func _ready():
 	return
-	
-#This should contain created_relations["CLUEID"]["children"] will give children (curr node affects
-#a child's score), and ...["parents"] will give parents (the nodes that affect curr's score)
+
 var created_relations = {
 }
 
-const SUSPECT_THRESHOLD = 80 #switch to 100
+const SUSPECT_THRESHOLD = 90 #switch to 100
 const default = "This doesn't make sense to me..."
-const suspect = "Found a suspect!"
-var first_suspect = "Found a suspect! When this clue has more than " + str(SUSPECT_THRESHOLD) + " points, you can accuse the suspect."
-
-#When checking the relations of two facts, 
-#	input: fact_relations["CLUEID"]["AND"/"CONTRADICTS"/"SUPPORTS"]["clues"]
-#Example: fact_relations["CLUEID"]["AND"]["clues"] gives one or more valid clues for that relation
-#       Then, fact_relations["CLUEID"]["points"] gives the feasability points for that clue
-#		Finally, factRelations["CLUEID"]["AND"]["result"] gives a resultant fact for "AND"
-#If clue not found in "clues" array for a relation, give default response.
-var fact_relations = {
-	"tier3_test" : {
-		"and": { "clues": ["checkers_work_for_Fat_Cat"],
-		"result" : "tier3_test2"},
-		"points" : 1 },
-	"tier3_test2" : {
-		"points" : -1
-	},
-	"all_cats_work_for_Fat_Cat" : {
-		"and": { "clues" : ["checkers_is_cat"],
-		"result" :"checkers_work_for_Fat_Cat" },
-		"points" : 60 },
-	"checkers_is_cat" : {
-		"and" : { "clues" : ["all_cats_work_for_Fat_Cat"],
-		"result" : "checkers_work_for_Fat_Cat" },
-		"points": 90 },
-	"checkers_work_for_Fat_Cat" : {
-		"points": -1
-	},
-	"employee_badge_no_pic" : {
-		"contradicts": { "clues" : ["found_employee_badge"]},
-		"points" : 50
-	},
-	"found_employee_badge" : {
-		"supports" : { "clues" : ["checkers_work_for_Fat_Cat"]},
-		"points" : 100
-	},
+const suspect = "Looks like we found a new suspect we can accuse..."
 	
 	#Scenario 0 Relations
-	"general2" : {
-		"and" : { "clues" : ["bee1", "archie4", "archie6", "eyy1", "bee2"],
-		"result" : ["general3", "general3", "general4", "general5", "general5"]},
+	"general1" : {
+		"and" : { "clues" : ["archie3", "B1", "A1", "archie4"],
+		"result" : ["general2", "general2", "general4", "general3"] },
 		"points" : 100
+	},
+	"general2" : {
+		"supports" : { "clues" : ["suspectB"] },
+		"points" : 20
 	},
 	"general3" : {
-		"supports" : { "clues" : ["general9"] },
-		"points" : -1
+		"supports" : { "clues" : ["suspectArchie"] },
+		"points" : 20
 	},
 	"general4" : {
-		"points" : -1
+		"supports" : { "clues" : ["suspectA"] },
+		"points" : 20
 	},
 	"general5" : {
-		"contradicts" : { "clues" : ["general7", "general9"] },
-		"points" : -1
-	},
-	"general7" : {
 		"and" : { "clues" : ["office2"],
-		"result" : ["general9"] },
-		"contradicts" : { "clues" : ["general5"] },
+		"result" : ["general6"] },
 		"points" : -1
 	},
-	"general9" : {
-		"supports" : { "clues" : ["general3"] },
-		"contradicts" : { "clues" : ["general5"] },
+	"general6" : {
+		"supports" : { "clues" : ["suspectB"] },
 		"points" : -1
-	},
-
-	"me1" : {
-		"points" : 100
 	},
 
 	"archie1" : { 
-		"and" : { "clues" : ["archie3"],
-		"result" : ["general2"]},
+		"and" : { "clues" : ["archie2"],
+		"result" : ["general1"]},
 		"supports" : { "clues" : ["eyy1"] },
-		"points" : 90
+		"points" : 10
+	},
+	"archie2" : { 
+		"and" : { "clues" : ["archie1"],
+		"result" : ["general1"] },
+		"points" : 100
 	},
 	"archie3" : { 
-		"and" : { "clues" : ["archie1"],
+		"and" : { "clues": ["general1"],
 		"result" : ["general2"] },
-		"points" : 90
+		"supports" : { "clues": ["suspectB"] },
+		"points" : 10
 	},
-	"archie4" : { 
-		"and" : { "clues": ["general2"],
+	"archie4" : {
+		"and" : { "clues" : ["general1"],
 		"result" : ["general3"] },
-		"supports" : { "clues": ["bee1"] },
-		"contradicts" : { "clues" : ["eyy1"] },
-		"points" : 90
-	},
-	"archie6" : {
-		"and" : { "clues" : ["general2"],
-		"result" : ["general4"] },
-		"points" : 99
+		"contradicts" : { "clues" : ["suspectArchie"] },
+		"points" : 10
 	},
 
-	"eyy1" : {
-		"and" : { "clues" : ["general2"],
-		"result" : ["general5"] },
-		"supports" : { "clues" : ["archie1", "bee2"] },
-		"points" : 90
+	"A1" : {
+		"and" : { "clues" : ["general1"],
+		"result" : ["general4"] },
+		"supports" : { "clues" : ["suspectA"] },
+		"points" : 10
 	},
-	"eyy2" : {
-		"contradicts" : { "clues" : ["general4", "general5", "bee3"] },
-		"points" : 90
+	"A2" : {
+		"contradicts" : { "clues" : ["suspectA"] },
+		"points" : 10
 	},
-	"eyy6" : {
-		"supports" : { "clues" : ["archie4", "bee1"] },
-		"contradicts" : { "clues" : ["bee3"] },
-		"points" : 90
+	"A3" : {
+		"supports" : { "clues" : ["suspectB"] },
+		"points" : 10
 	},
-	"library4" : {
+
+	"B1" : {
+		"and" : { "clues" : ["general1"],
+		"result" : ["general2"]},
+		"points" : 100
+	},
+	"B2" : { 
+		"supports" : { "clues" : ["suspectA"] },
+		"contradicts" : { "clues" : ["suspectB"] },
+		"points" : 20
+	},
+
+	"library1" : {
 		"and" : { "clues" : ["office1"],
-		"result" : ["general7"] },
-		"points" : 50
+		"result" : ["general5"] },
+		"points" : 100
 	},
 	"kitchen1" : {
-		"supports" : { "clues" : ["bee1"]},
-		"points" : 99
-	},
-	"bee1" : { 
-		"supports" : { "clues" : ["archie4"] },
-		"and" : { "clues" : ["general2"],
-		"result" : ["general3"]},
-		"points" : 90
-	},
-	"bee2" : { 
-		"supports" : { "clues" : ["eyy1"] },
-		"contradicts" : { "clues" : ["general4"] },
-		"and" : { "clues" : ["general2"],
-		"result" : ["general5"] },
-		"points" : 90
-	},
-	"bee3" : {
-		"contradicts" : { "clues" : ["eyy6", "eyy2", "general3"] },
-		"points" : 90
+		"supports" : { "clues" : ["suspectB"]},
+		"points" : 5
 	},
 	"office1" : {
-		"and" : { "clues" : ["library4"],
-		"result" : ["general7"] },
-		"supports" : { "clues" : ["general5"] },
-		"points" : 99
+		"and" : { "clues" : ["library1"],
+		"result" : ["general5"] },
+		"supports" : { "clues" : ["suspectA"]},
+		"points" : 40
 	},
 	"office2" : {
-		"and" : { "clues" : ["general7"],
-		"result" : ["general9"] },
-		"points" : 99
+		"and" : { "clues" : ["general5"],
+		"result" : ["general6"] },
+		"points" : 10
 	},
 }
